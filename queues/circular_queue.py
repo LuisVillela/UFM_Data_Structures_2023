49b6844827c59deddd9633c2eaa46822ca3d59a2
LuisVillela <103969295+LuisVillela@users.noreply.github.com>
2023-02-23 11:16:07 -0600

    Update circular_queue.py
    added constructor | added front, rear, max...

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -12,3 +12,14 @@ class CircularQueue:
         max (int): maximum amount of elements in queue
         count (int): current amount of elements in queue
     '''
+
+    def __init__(self, size: int) -> None:
+        self.elements = [None] * size
+        self.front = -1
+        self.rear = -1
+        self.max = size
+        self.count = 0
+
+
+    def __repr__(self) -> str:
+        return 'Current queue: {} | Front: {} | Rear: {}'.format(self.elements, self.front, self.rear)
