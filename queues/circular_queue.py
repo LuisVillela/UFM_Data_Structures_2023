5b81a5b031bbf141ba4d37c4a9463171959c1b0e
LuisVillela <103969295+LuisVillela@users.noreply.github.com>
2023-02-23 13:00:30 -0600

    Update circular_queue.py
    add enqueue

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -23,3 +23,27 @@ class CircularQueue:
 
     def __repr__(self) -> str:
         return 'Current queue: {} | Front: {} | Rear: {}'.format(self.elements, self.front, self.rear)
+
+    def enqueue(self, value: str) -> None:
+        '''
+        Inserts element into the queue.
+
+        Args:
+            value (str): value to be enqueued
+
+        Returns:
+            None
+        '''
+
+        if self.count == self.max:
+            print("Queue Overflow...")
+            return None
+        
+        if self.front == -1 and self.rear == -1:
+            self.front = 0
+            self.rear = 0
+        else:
+            self.rear = (self.rear + 1) % self.max
+
+        self.elements[self.rear] = value
+        self.count += 1
